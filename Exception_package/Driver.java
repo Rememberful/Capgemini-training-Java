b779530947e26708fd8b0d91a12f163f2dd5fcb9
Aditya Kumar <adii.utsav@gmail.com>
2026-01-22 16:17:30 +0530

    Studied about wrapper class

--- Exception_package/Driver.java
+++ Exception_package/Driver.java
@@ -1,6 +1,40 @@
 package Exception_package;
 
 public class Driver {
-	int age = -10;
-	
-}
+	public static void main(String[] args) {
+//		//m1();
+//		String s="xyz";
+//		while(true) {
+//			s=s+s;//OutOfMemoryError
+//		}
+//	}
+//	public static void m1() {
+//		System.out.println("hi");
+//		m1();//stack over flow error
+//	}
+		
+		//wrapper : as it is wrapping the primitive to NP and vice-versa
+//		int n = 10;
+//		Integer i = n; //Auto-boxing -> primitive is converted to object
+//		int n2 = i; // Un-boxing as we again made it Primitive 
+		
+//		Integer i2 = 90; //auto boxing, as 90 by default be integer (primitive) and which get converted to Non-primitive
+//		
+//		Character c1 = 'a';
+//		System.out.println(c1);
+//		
+//		System.out.println(Integer.MIN_VALUE);
+//		System.out.println(Integer.MAX_VALUE);
+//		
+		//string to integer
+//		System.out.println(Integer.valueOf("123"));
+//		Integer i = Integer.valueOf("123"); //.valueOf converts string to integer
+//		System.out.println(i+1);
+		
+//		Boolean b = Boolean.valueOf("tRue"); //if it is not boolean then it will return false 
+//		System.out.println(b);
+		
+		int s = Integer.parseInt("12");// convert string to int
+		System.out.println(s);
+		
+}}
