52c4b827cec0050afc1be39260be8fb5b7a344a1
Aditya Kumar <adii.utsav@gmail.com>
2026-01-27 10:09:42 +0530

    Studied about vector

---
+++ Linked_list/VectorExample.java
@@ -0,0 +1,17 @@
+package Linked_list;
+
+import java.util.Vector;
+
+public class VectorExample {
+	 public static void main(String[] args) {
+
+	        Vector<String> vector = new Vector<>();
+
+	        vector.add("A");
+	        vector.add("B");
+	        vector.add("C");
+
+	        System.out.println(vector.get(0));   // Access element
+	        System.out.println(vector);          // Print all elements
+	    }
+}
